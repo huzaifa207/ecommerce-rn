d75f39c62465ac262abc6da9869358d2d31a6d61
huzaifa <khanhuzaifa207@gmail.com>
2021-06-28 17:15:49 +0500

    Update: modified App.js

--- App.js
+++ App.js
@@ -1,29 +1,12 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 
 import Drawer from "./navigations/Drawer";
-import Stack from "./navigations/Stack";
-
-import AllProducts from "./screens/AllProducts";
 
 export default function App() {
   return (
     <NavigationContainer>
       <Drawer />
     </NavigationContainer>
-    // <View>
-    //   <AllProducts />
-    // </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
